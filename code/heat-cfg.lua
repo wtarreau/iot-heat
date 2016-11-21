819cd6087ea1f4f668bfc48c9d197324aae8d20c
Willy Tarreau <w@1wt.eu>
2016-11-21 09:44:35 +0100

    code: heat-cfg: provide example heat config file for a living room
    This is for a living room, in short, STRONG heating in the morning,
    ECO mode when away, NORMAL heating when there's light, ECO for the
    rest.

---
+++ code/heat-cfg.lua
@@ -0,0 +1,17 @@
+heat_pwm_ECO=1023
+heat_pwm_NORMAL=500
+heat_pwm_STRONG=250
+heat_pwm_FAST=0
+
+heat_p_NIGHT_LIT="NORMAL"
+heat_p_NIGHT_DRK="ECO"
+heat_p_MORNING_LIT="STRONG"
+heat_p_MORNING_DRK="STRONG"
+heat_p_ATHOME_LIT="NORMAL"
+heat_p_ATHOME_DRK="NORMAL"
+heat_p_AWAY_LIT="NORMAL"
+heat_p_AWAY_DRK="ECO"
+heat_p_EVENING_LIT="NORMAL"
+heat_p_EVENING_DRK="NORMAL"
+heat_p_TOBED_LIT="NORMAL"
+heat_p_TOBED_DRK="ECO"
